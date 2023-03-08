8ec645eced7bbc762256d107005660f91499efdc
bijokmathew <bijo.nbr@hotmail.com>
2023-03-08 11:27:57 +0000

    Added add to bag functionality

--- products/views.py
+++ products/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404, redirect, reverse
 from django.contrib import messages
 from django.db.models import Q
+from django.db.models.functions import Lower
 from .models import Product, Category
 
 
@@ -11,11 +12,34 @@ def all_products(request):
     products = Product.objects.all()
     query = None
     category_list = None
+    sort = None
+    direction = None
     if request.GET:
+
         if 'category' in request.GET:
             category_list = request.GET['category'].split(',')
             products = Product.objects.filter(category__name__in=category_list)
+            print("inside cat category_list= ", category_list)
             category_list = Category.objects.filter(name__in=category_list)
+
+        if 'sort' in request.GET:
+            sortkey = request.GET['sort']
+            sort = sortkey
+            print("sortkey =", sortkey)
+            if sortkey == 'name':
+                sortkey = 'lower_name'
+                products = products.annotate(lower_name=Lower('name'))
+                for product in products:
+                    print("products", product.price)
+
+            if 'direction' in request.GET:
+                direction = request.GET['direction']
+                if direction == 'desc':
+                    sortkey = f'-{sortkey}'
+                print("sortkey f=", sortkey)
+            products = products.order_by(sortkey)
+            for product in products:
+                print("products -last", product.price)
 
         if 'q' in request.GET:
             query = request.GET['q']
@@ -26,9 +50,13 @@ def all_products(request):
                 queries = Q(name__icontains=query) | Q(description__icontains=query)
                 products = Product.objects.filter(queries)
 
+    current_sorting = f'{sort}_{direction}'
+    print("current_sorting =", current_sorting)
+    print("current_category =", category_list)
     context = {
         'products': products,
         'current_category': category_list,
+        'current_sorting': current_sorting,
     }
     return render(request, "products/products.html", context)
 
@@ -41,4 +69,4 @@ def product_detail(request, product_id):
     context = {
         'product': product,
     }
-    return render(request, "products/product_detail.html", context)
+    return render(request, "products/product_detail.html", context)    
