520f897eb98ebadda942af42b82f127302aa033b
bijokmathew <bijo.nbr@hotmail.com>
2023-03-08 16:33:40 +0000

    updated contexts processor

--- bag/views.py
+++ bag/views.py
@@ -12,16 +12,15 @@ def add_to_bag(request, item_id):
     """
     Add a quantity of the specified item to the shopping bag
     """
-    print("url =", request.path_info)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect-url')
     bag = request.session.get('bag', {})
 
     if item_id in list(bag.keys()):
-        bag['item_id'] += quantity
+        bag[item_id] += quantity
     else:
-        bag['item_id'] = quantity
+        bag[item_id] = quantity
 
     request.session['bag'] = bag
     print("bag =", request.session['bag'])
-    return redirect(redirect_url)    
+    return redirect(redirect_url)
